8a8bf49a244901e4242fd83a1c22db868c60969d
aleksei z <farguzalexey@gmail.com>
2026-02-22 15:05:08 +0300

    add django project

---
+++ custom_auth_system/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.decorators.http import require_GET
+
+
+@require_GET
+def index(request):
+    return render(
+        request,
+        'index.html',
+    )
