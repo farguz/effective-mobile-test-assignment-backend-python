4d9c068d8ba267b90055cf3182c10bb783714a99
aleksei z <farguzalexey@gmail.com>
2026-02-22 21:10:06 +0300

    add custom user and custom admin models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'first_name', 'last_name', 'is_active', ]
+    search_fields = ['email', 'first_name', 'last_name', ]
+    ordering = ['email', ]
+    
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    username = None
+    first_name = models.CharField(
+        max_length=150,
+        blank=False
+        )
+    middle_name = models.CharField(
+        max_length=150,
+        blank=False
+        )
+    last_name = models.CharField(
+        max_length=150,
+        blank=False
+        )
+    email = models.EmailField(
+        unique=True,
+        blank=False
+        )
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def get_full_name(self):
+        """
+        extends original func with middle_name
+        """
+        full_name = f'{self.last_name} {self.middle_name} {self.first_name}'
+        return full_name.strip()
+
+    def __str__(self):
+        return f'{self.get_full_name()}: {self.email}'
