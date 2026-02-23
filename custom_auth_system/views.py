33d185058ace45585b51c628b58848f825a12439
aleksei z <farguzalexey@gmail.com>
2026-02-23 14:47:59 +0300

    add login and logout form/views

--- custom_auth_system/views.py
+++ custom_auth_system/views.py
@@ -1,5 +1,11 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.decorators.http import require_GET
+
+from .forms import LoginForm
 
 
 @require_GET
@@ -8,3 +14,22 @@ def index(request):
         request,
         'index.html',
     )
+
+
+class AuthSystemLoginView(LoginView):
+    form_class = LoginForm
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        messages.success(self.request, 'Login successful')
+        return reverse_lazy('index_page')
+         
+
+class AuthSystemLogoutView(LoginRequiredMixin, LogoutView):
+
+    next_page = reverse_lazy('index_page')
+
+    def post(self, request, *args, **kwargs):
+        messages.info(request, 'Logout successful')
+        return super().post(request, *args, **kwargs)
