53a45268fc04b538fde60987162676f3353cfa91
aleksei z <farguzalexey@gmail.com>
2026-02-23 11:58:36 +0300

    add role, resource, permisssion models

--- custom_auth_system/users/models.py
+++ custom_auth_system/users/models.py
@@ -4,6 +4,52 @@ from django.db import models
 # Create your models here.
 
 
+class Role(models.Model):
+    name = models.CharField(
+        max_length=80,
+        unique=True,
+        verbose_name='Role name'
+        )
+
+    def __str__(self):
+        return self.name
+    
+
+class Resource(models.Model):
+    name = models.CharField(
+        max_length=80,
+        unique=True,
+        verbose_name='Resource name'
+        )
+
+    def __str__(self):
+        return self.name
+    
+
+class Permission(models.Model):
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.CASCADE,
+        related_name='permissions'
+        )
+    resource = models.ForeignKey(
+        Resource,
+        on_delete=models.CASCADE,
+        related_name='permissions'
+        )
+    can_read = models.BooleanField(default=False)
+    can_create = models.BooleanField(default=False)
+    can_update = models.BooleanField(default=False)
+    can_delete = models.BooleanField(default=False)
+    can_soft_delete = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ['role', 'resource']
+
+    def __str__(self):
+        return f'{self.role}: {self.resource}'
+
+
 class CustomUser(AbstractUser):
     username = None
     first_name = models.CharField(
@@ -22,7 +68,14 @@ class CustomUser(AbstractUser):
         unique=True,
         blank=False
         )
-    
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+        )
+    updated_at = models.DateTimeField(auto_now=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'middle_name', 'last_name', ]
 
@@ -35,5 +88,3 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return f'{self.get_full_name()}: {self.email}'
-    
-    
