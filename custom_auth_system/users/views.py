7055b4fc34f46b3a81d141cada4e7a60144a66a0
aleksei z <farguzalexey@gmail.com>
2026-02-23 14:49:51 +0300

    feat: create user (registration)

--- custom_auth_system/users/views.py
+++ custom_auth_system/users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
-# Create your views here.
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+User = get_user_model()
+
+
+class RegistrationView(CreateView):
+
+    model = User
+    form_class = CustomUserCreationForm
+    template_name = 'users/create.html'
+    success_url = reverse_lazy('login')
+
+    def get_success_url(self):
+        messages.success(self.request, 'User created successfully')
+        return super().get_success_url()
+
+
+class UpdateUserView(UpdateView):
+    model = User
+    form_class = CustomUserChangeForm
+    template_name = 'users/update.html'
+    pass
+
+
+class DeleteUserView(DeleteView):
+
+    model = User
+    template_name = 'users/delete.html'
+    pass
