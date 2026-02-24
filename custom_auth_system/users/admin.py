2b29cee552a21f2bf33df5e587a2b568f77ff8d0
aleksei z <farguzalexey@gmail.com>
2026-02-24 13:29:18 +0300

    update fieldsets, now its ok to create user via admin panel

--- custom_auth_system/users/admin.py
+++ custom_auth_system/users/admin.py
@@ -1,15 +1,30 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import CustomUser, Permission, Resource, Role
 
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = CustomUser
+
     list_display = ['email', 'first_name', 'last_name', 'role', 'is_active', ]
     search_fields = ['email', 'first_name', 'last_name', ]
     ordering = ['email', ]
+
+    fieldsets = [
+        (None, {'fields': ('email', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'middle_name', 'last_name', 'role')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (('Important dates'), {'fields': ('last_login',)}),
+    ]
+
+    # change field password to psw1, psw2
+    fieldsets[0][-1]['fields'] = ('email', 'password1', 'password2')
+    add_fieldsets = fieldsets
     
 
 admin.site.register(CustomUser, CustomUserAdmin)
