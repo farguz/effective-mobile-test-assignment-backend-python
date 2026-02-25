7904856ef5c7d6c2bec7a7c57bada35491c759cb
aleksei z <farguzalexey@gmail.com>
2026-02-25 10:04:28 +0300

    add permission pages urls

--- custom_auth_system/users/urls.py
+++ custom_auth_system/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from . import views_admin
 from .views import (
     DeleteUserView,
     RegistrationView,
@@ -11,4 +12,12 @@ urlpatterns = [
     path('<int:pk>/update/', UpdateUserView.as_view(), name='user_update'),
     path('<int:pk>/delete/', DeleteUserView.as_view(), name='user_delete'),
     path('create/', RegistrationView.as_view(), name='user_create'),
+]
+
+# ../permissions/
+urlpatterns += [
+    path('permissions/', views_admin.permission_list, name='permission_list'),
+    path('permissions/<int:permission_id>/update/', views_admin.permission_update, name='permission_update'),
+    path('api/permissions/', views_admin.permission_list_api),
+    path('api/permissions/<int:permission_id>/', views_admin.permission_update_api),
 ]
