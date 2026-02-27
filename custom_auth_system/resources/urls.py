706768cfcc6bc3d3efef268cdb5ec02fcd5ff33f
aleksei z <farguzalexey@gmail.com>
2026-02-27 09:24:47 +0300

    add urls to index views of different resource types

--- custom_auth_system/resources/urls.py
+++ custom_auth_system/resources/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 # ../resources/
 urlpatterns = [
-    path('courses/', views.course_list, name='course_list'),
-    path('solutions/', views.solution_detail, name='solution_detail'),
+    path('', views.resources_index, name='resources_index'),
+    path('courses/', views.courses_list, name='courses_list'),
+    path('lessons/', views.lessons_list, name='lessons_list'),
+    path('tests/', views.tests_list, name='tests_list'),
+    path('solutions/', views.solutions_list, name='solutions_list'),
+    path('professions/', views.professions_list, name='professions_list'),
 ]
