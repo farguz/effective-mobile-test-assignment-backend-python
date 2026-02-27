21af5f2e75679ff93514de70fe9dee62f54b39e2
aleksei z <farguzalexey@gmail.com>
2026-02-27 16:44:25 +0300

    add basic tests which use sqlite db

--- custom_auth_system/settings.py
+++ custom_auth_system/settings.py
@@ -136,3 +136,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+import sys
+
+if 'pytest' in sys.modules:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
--- custom_auth_system/users/tests.py
+++ custom_auth_system/users/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+from custom_auth_system.users.models import CustomUser
+
+
+@pytest.mark.django_db
+class TestUserAuth:
+    
+    @pytest.fixture
+    def user_data(self):
+        return {
+            'email': 'test@gmail.com',
+            'password': 'test@gmail.comtest@gmail.com',
+            'first_name': 'Ivan',
+            'middle_name': 'Ivanovich',
+            'last_name': 'Ivanov'
+        }
+
+    @pytest.fixture
+    def active_user(self, user_data):
+        return CustomUser.objects.create_user(**user_data)
+
+    @pytest.fixture
+    def inactive_user(self, user_data):
+        user_data['email'] = 'inactive@gmail.com'
+        user = CustomUser.objects.create_user(**user_data)
+        user.is_active = False
+        user.save()
+        return user
+
+    def test_create_user(self, active_user):
+        assert active_user.email == 'test@gmail.com'
+        assert active_user.is_active is True
+
+    def test_login_success(self, client, active_user, user_data):
+        url = reverse('login')
+        response = client.post(url, {
+            'email': user_data['email'],
+            'password': user_data['password']
+        })
+        
+        assert response.status_code == 302
+        assert response.url == reverse('index_page')
+        assert 'access_token' in client.cookies
+        assert 'refresh_token' in client.cookies
+
+    def test_login_inactive_user(self, client, inactive_user, user_data):
+        url = reverse('login')
+        response = client.post(url, {
+            'email': inactive_user.email,
+            'password': user_data['password']
+        })
+        
+        assert response.status_code == 200
+        assert 'has been deleted' in response.content.decode('utf-8')
+        assert 'access_token' not in client.cookies
+
+    def test_login_invalid_password(self, client, active_user):
+        url = reverse('login')
+        response = client.post(url, {
+            'email': active_user.email,
+            'password': 'wrongpassword'
+        })
+        
+        assert response.status_code == 200
+        assert 'Invalid credentials' in response.content.decode('utf-8')
